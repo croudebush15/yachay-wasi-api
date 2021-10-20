1fb4dae3f1aaf6ff0844726b30434949bcf9ff12
unknown <croudebush15@gmail.com>
2021-10-20 10:34:23 -0500

    Controles admin para mant curso

--- src/main/java/com/pe/proyectotechnologico/Controller/CourseController.java
+++ src/main/java/com/pe/proyectotechnologico/Controller/CourseController.java
@@ -1,32 +1,38 @@
 package com.pe.proyectotechnologico.Controller;
 
-
 import com.pe.proyectotechnologico.Model.Course;
 import com.pe.proyectotechnologico.Service.CourseService;
+import com.pe.proyectotechnologico.Service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
 @RequestMapping("/course")
 public class CourseController {
 
+    final UserService userService;
     private final CourseService courseService;
 
 
-    public CourseController(CourseService courseService) {
+    public CourseController(CourseService courseService, UserService userService) {
         this.courseService = courseService;
+        this.userService = userService;
     }
 
     @GetMapping
-    public ResponseEntity<List<Course>> getCourses(){
+    public ResponseEntity<List<Course>> getCourses(HttpServletRequest request){
+        if (!userService.isUserAdmin(request)) return new ResponseEntity(HttpStatus.FORBIDDEN);
         return new ResponseEntity<>(courseService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> getCourse(@PathVariable Integer id){
+    public ResponseEntity<Course> getCourse(HttpServletRequest request,
+                                            @PathVariable Integer id){
+        if (!userService.isUserAdmin(request)) return new ResponseEntity(HttpStatus.FORBIDDEN);
         Course currentCourse = courseService.findById(id);
         if( null == currentCourse){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -35,25 +41,29 @@ public class CourseController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<Course> postCourse(@RequestBody Course course){
+    public ResponseEntity<Course> postCourse(HttpServletRequest request,
+                                             @RequestBody Course course){
+        if (!userService.isUserAdmin(request)) return new ResponseEntity(HttpStatus.FORBIDDEN);
         courseService.create(course);
         return new ResponseEntity<>(course,HttpStatus.CREATED);
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<Course> putCourse(@PathVariable Integer id,
+    @PutMapping("/update")
+    public ResponseEntity<Course> putCourse(HttpServletRequest request,
                                               @RequestBody Course course){
-
-        if( null == courseService.findById(id)){
+        if (!userService.isUserAdmin(request)) return new ResponseEntity(HttpStatus.FORBIDDEN);
+        if( null == courseService.findById(course.getId())){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        course.setId(id);
+        course.setId(course.getId());
         courseService.update(course);
         return new ResponseEntity<>(course,HttpStatus.OK);
     }
 
     @DeleteMapping("delete/{id}")
-    public ResponseEntity<Course> deleteCourse(@PathVariable Integer id){
+    public ResponseEntity<Course> deleteCourse(HttpServletRequest request,
+                                               @PathVariable Integer id){
+        if (!userService.isUserAdmin(request)) return new ResponseEntity(HttpStatus.FORBIDDEN);
         if( null == courseService.findById(id)){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
