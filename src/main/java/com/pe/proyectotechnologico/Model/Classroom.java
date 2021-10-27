aefb03bf9117e0f8b4d0b4ea2ca7f32970d68195
unknown <croudebush15@gmail.com>
2021-10-27 12:12:54 -0500

    Autogenerar NRC

--- src/main/java/com/pe/proyectotechnologico/Model/Classroom.java
+++ src/main/java/com/pe/proyectotechnologico/Model/Classroom.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Generated;
 import org.hibernate.annotations.GenerationTime;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
@@ -22,7 +23,16 @@ import static javax.persistence.GenerationType.IDENTITY;
 @Table(name = "classroom")
 public class Classroom {
     @Id
-    @GeneratedValue(strategy = IDENTITY)
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "nrc_sequence"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "100"),
+                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
+            }
+    )
     @Column(name = "idClassroom", unique = true, nullable = false)
     private Integer id;
     private String name;
