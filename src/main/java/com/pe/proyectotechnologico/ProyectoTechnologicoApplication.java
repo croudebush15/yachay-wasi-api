53e92f1c7d0c1564d2f3397b4ceb9e84be91631d
unknown <croudebush15@gmail.com>
2021-12-10 18:45:33 -0500

    fix main

--- src/main/java/com/pe/proyectotechnologico/ProyectoTechnologicoApplication.java
+++ src/main/java/com/pe/proyectotechnologico/ProyectoTechnologicoApplication.java
@@ -1,35 +1,14 @@
 package com.pe.proyectotechnologico;
 
-import com.pe.proyectotechnologico.Model.Classroom;
-import com.pe.proyectotechnologico.Model.Teacher;
-import com.pe.proyectotechnologico.Model.User;
-import com.pe.proyectotechnologico.Service.ClassroomService;
-import com.pe.proyectotechnologico.Service.LessonService;
-import com.pe.proyectotechnologico.Service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Date;
 
 @SpringBootApplication
-public class ProyectoTechnologicoApplication implements CommandLineRunner {
+public class ProyectoTechnologicoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProyectoTechnologicoApplication.class, args);
-    }
-
-    @Autowired
-    LessonService lessonService;
-
-    @Autowired
-    ClassroomService classroomService;
-
-    @Override
-    public void run(String... args) throws Exception {
-        /*Classroom classroom = classroomService.findById(101);
-        lessonService.generateWeeklyLessonsForClassroom(classroom,15,"2021-12-06");*/
-
     }
 }
